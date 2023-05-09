dfba22dddce69e252921b4e9f682e113185bf7b0
Anushalokiny <anushalokiny@gmail.com>
2023-05-09 19:13:49 +0530

    first commit

---
+++ src/main/java/com/devops/anushadevopsmaven/AnushaDevopsMavenApplication.java
@@ -0,0 +1,13 @@
+package com.devops.anushadevopsmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnushaDevopsMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnushaDevopsMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/anushadevopsmaven/AnushaDevopsMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.anushadevopsmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnushaDevopsMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
