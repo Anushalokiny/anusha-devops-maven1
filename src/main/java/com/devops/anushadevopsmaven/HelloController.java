3ba186d16eca2400add69cd06011c1955d623092
Anushalokiny <anushalokiny@gmail.com>
2023-05-15 19:50:19 +0530

    commit changes

--- src/main/java/com/devops/anushadevopsmaven/HelloController.java
+++ src/main/java/com/devops/anushadevopsmaven/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
   
   @RequestMapping("/")
   public String index() {
-      return "Greetings from Python Life DevOPS assignment ";
+      return "Greetings from Python Life DevOPS lab assignment ";
   }
 
 }
